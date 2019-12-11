a071bd5ee00c5c0106bcca2d9002723ef581402a
jpergosky <57735849+jpergosky@users.noreply.github.com>
2019-12-10 21:01:35 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    //return (
+    // <div className="App">
+    //  <h1>Hi, I'm a React App</h1>
+    // </div>
+    // );
+    return React.createElement('div', null, "h1", 'Hi, I\'m a React App!!!');
+  }
+}
